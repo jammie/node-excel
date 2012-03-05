1311bff530f9eb383755dd70058a102b8fc802ac
Tom Smith <tom.smith@clock.co.uk>
2012-03-05 18:27:43 +0000

    Testing email notifications work on build failure

---
+++ test/worksheet.test.js
@@ -0,0 +1,12 @@
+describe('worksheet', function() {
+
+	describe('#create()', function() {
+
+		it('returns an empty worksheet', function(done) {
+ [1,2,3].indexOf(0).should.equal(-1);
+		});
+
+
+	});
+
+});
