b49941c4c40342be2cedd83aa9489591c8f2f215
Tom Smith <tom.smith@clock.co.uk>
2012-03-05 18:30:59 +0000

    Altering package.json and removing dummy worksheet assertion

--- test/worksheet.test.js
+++ test/worksheet.test.js
@@ -2,8 +2,7 @@ describe('worksheet', function() {
 
 	describe('#create()', function() {
 
-		it('returns an empty worksheet', function(done) {
- [1,2,3].indexOf(0).should.equal(-1);
+		it('returns an empty worksheet', function() {
 		});
 
 
