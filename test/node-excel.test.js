0bd5acdcc741afd8b5f247ce0a9b9245a1e7fc69
Adam Duncan <adam.duncan@clock.co.uk>
2012-03-06 22:07:02 +0000

    Added new functions and dummy tests

--- test/node-excel.test.js
+++ test/node-excel.test.js
@@ -18,13 +18,60 @@ describe('Node Excel', function() {
 		});
 	});
 
+	describe('#getAllSheets()', function() {
+		it('returns all worksheets as an array');
+	});
+
 	describe('#getSheetCount()', function() {
 		it('return the number of worksheets in the current workbook');
+	});	
+
+	describe('#getSheet()', function() {
+		it('return the sheet at the specified index');
+		it('throw an error if specified index does not exist');
+	});
+
+	describe('#getActiveSheet()', function() {
+		it('return the currently active worksheet');
+	});
+
+	describe('#setActiveSheetIndex()', function() {
+		it('sets the currently active sheet to the given index and returns new active sheet');
+	});
+
+	describe('#setActiveSheetIndexByName()', function() {
+		it('sets the currently active sheet by sheet name and returns new active sheet');
 	});
 
 	describe('#getSheetByName()', function() {
 		it('return a worksheet object with a valid name');
 		it('return null for a non existent name');
 	});
+
+	describe('#createSheet()', function() {
+		it('returns a new sheet object');
+	});
+
+	describe('#addSheet()', function() {
+		it('adds a new sheet object at the specified index');
+	});
+
+	describe('#removeSheetByIndex()', function() {
+		it('removes the sheet at the given index');
+		it('throw an error if specified index does not exist');
+	});
+
+	describe('#getIndex()', function() {
+		it('returns the index for a given sheet');
+	});
+
+	describe('#setIndexByName()', function() {
+		it('set index for sheet by sheet name and return newIndex');
+	});
+
+	describe('#getSheetNames()', function() {
+		it('return an array of sheet names');
+	});
+
 
 });
