db85d34568ebfd30e73c8bf92cf96ff61150fd9f
Tom Smith <tom.smith@clock.co.uk>
2012-03-15 18:33:55 +0000

    Adding Cell classes and mocha skelton classes for all functions

---
+++ test/cell.test.js
@@ -0,0 +1,174 @@
+describe('Cell', function() {
+
+	describe('#create()', function() {
+		it('todo');
+	});
+
+	describe('#notifyCacheController()', function() {
+		it('todo');
+	});
+
+	describe('#detach()', function() {
+		it('todo');
+	});
+
+	describe('#attach()', function() {
+		it('todo');
+	});
+
+	describe('#getColumn()', function() {
+		it('todo');
+	});
+
+	describe('#getRow()', function() {
+		it('todo');
+	});
+
+	describe('#getCoordinate()', function() {
+		it('todo');
+	});
+
+	describe('#getValue()', function() {
+		it('todo');
+	});
+
+	describe('#getFormattedValue()', function() {
+		it('todo');
+	});
+
+	describe('#setValue()', function() {
+		it('todo');
+	});
+
+	describe('#setValueExplicit()', function() {
+		it('todo');
+	});
+
+	describe('#getCalculatedValue()', function() {
+		it('todo');
+	});
+
+	describe('#setCalculatedValue()', function() {
+		it('todo');
+	});
+
+	describe('#getOldCalculatedValue()', function() {
+		it('todo');
+	});
+
+	describe('#getDataType()', function() {
+		it('todo');
+	});
+
+	describe('#setDataType()', function() {
+		it('todo');
+	});
+
+	describe('#hasDataValidation()', function() {
+		it('todo');
+	});
+
+	describe('#getDataValidation()', function() {
+		it('todo');
+	});
+
+	describe('#setDataValidation()', function() {
+		it('todo');
+	});
+
+	describe('#hasHyperlink()', function() {
+		it('todo');
+	});
+
+	describe('#getHyperlink()', function() {
+		it('todo');
+	});
+
+	describe('#setHyperlink()', function() {
+		it('todo');
+	});
+
+	describe('#getParent()', function() {
+		it('todo');
+	});
+
+	describe('#rebindParent()', function() {
+		it('todo');
+	});
+
+	describe('#isInRange()', function() {
+		it('todo');
+	});
+
+	describe('#coordinateFromString()', function() {
+		it('todo');
+	});
+
+	describe('#absoluteReference()', function() {
+		it('todo');
+	});
+
+	describe('#absoluteCoordinate()', function() {
+		it('todo');
+	});
+
+	describe('#splitRange()', function() {
+		it('todo');
+	});
+
+	describe('#buildRange()', function() {
+		it('todo');
+	});
+
+	describe('#rangeBoundaries()', function() {
+		it('todo');
+	});
+
+	describe('#rangeDimension()', function() {
+		it('todo');
+	});
+
+	describe('#columnIndexFromString()', function() {
+		it('todo');
+	});
+
+	describe('#stringFromColumnIndex()', function() {
+		it('todo');
+	});
+
+	describe('#extractAllCellReferencesInRange()', function() {
+		it('todo');
+	});
+
+	describe('#compareCells()', function() {
+		it('todo');
+	});
+
+	describe('#getValueBinder()', function() {
+		it('todo');
+	});
+
+	describe('#setValueBinder()', function() {
+		it('todo');
+	});
+
+	describe('#__clone()', function() {
+		it('todo');
+	});
+
+	describe('#getXfIndex()', function() {
+		it('todo');
+	});
+
+	describe('#setXfIndex()', function() {
+		it('todo');
+	});
+
+	describe('#setFormulaAttributes()', function() {
+		it('todo');
+	});
+
+	describe('#getFormulaAttributes()', function() {
+		it('todo');
+	});
+});
---
+++ test/cell_data-type.test.js
@@ -0,0 +1,23 @@
+describe('Cell - Data Type', function() {
+
+	describe('#create()', function() {
+		it('todo');
+	});
+
+	describe('#getErrorCodes()', function() {
+		it('todo');
+	});
+
+	describe('#dataTypeForValue()', function() {
+		it('todo');
+	});
+
+	describe('#checkString()', function() {
+		it('todo');
+	});
+
+	describe('#checkErrorCode()', function() {
+		it('todo');
+	});
+
+});
