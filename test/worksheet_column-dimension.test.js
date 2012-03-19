7f7e649b2ba9f860546ed5f3fdce2709789999c0
Tom Smith <tom.smith@clock.co.uk>
2012-03-19 13:40:30 +0000

    Adding functions and tests for worksheet classes

--- test/worksheet_column-dimension.test.js
+++ test/worksheet_column-dimension.test.js
@@ -4,4 +4,33 @@ describe('WS - Column Dimension', function() {
 		it('todo');
 	});
 
+	describe('#getColumnIndex() && #setColumnIndex()', function() {
+		it('todo');
+	});
+
+	describe('#getWidth() && #setWidth()', function() {
+		it('todo');
+	});
+
+	describe('#getAutoSize() && #setAutoSize()', function() {
+		it('todo');
+	});
+
+	describe('#getVisible() && #setVisible()', function() {
+		it('todo');
+	});
+
+	describe('#getOutlineLevel() && #setOutlineLevel()', function() {
+		it('todo');
+	});
+
+	describe('#getCollapsed() && #setCollapsed()', function() {
+		it('todo');
+	});
+
+	describe('#getXfIndex() && #setXfIndex()', function() {
+		it('todo');
+	});
+
+
 });
--- test/worksheet_row-dimension.test.js
+++ test/worksheet_row-dimension.test.js
@@ -4,4 +4,28 @@ describe('WS - Row Dimension', function() {
 		it('todo');
 	});
 
+	describe('#getRowIndex() && #setRowIndex()', function() {
+		it('todo');
+	});
+
+	describe('#getRowHeight() && #setRowHeight()', function() {
+		it('todo');
+	});
+
+	describe('#getVisible() && #setVisible()', function() {
+		it('todo');
+	});
+
+	describe('#getOutlineLevel() && #setOutlineLevel()', function() {
+		it('todo');
+	});
+
+	describe('#getCollapsed() && #setCollapsed()', function() {
+		it('todo');
+	});
+
+	describe('#getXfIndex() && #setXfIndex()', function() {
+		it('todo');
+	});
+
 });
